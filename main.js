6f33638b05d7615ff7551643d1922f8925ee47ce
Aidan-Verdugo <aidan.william.verdugo@gmail.com>
2023-12-27 17:56:34 -0600

    Added import form page and started adding structure to file system. Also started the script that will load names and backgrounds on the main display page.

---
+++ main.js
@@ -0,0 +1,9 @@
+//varriables related to child info
+const childName = ["name1","name2","name3"];
+const theme = ["Dino","Art","Tech"]
+var childIndex = 0;
+
+//varriables related to csv file
+var file = "file.csv";
+
+document.getElementById("n1").innerHTML = childName[0];
