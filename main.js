6b835b26f9093864b9052172523f38bd8c299f23
Aidan-Verdugo <aidan.william.verdugo@gmail.com>
2023-12-28 18:07:14 -0600

    changed some file names finished name slides amoung other things

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 //varriables related to child info
-const childName = ["name1","name2","name3"];
-const theme = ["Dino","Art","Tech"]
-var childIndex = 0;
+let childName = ["name1","name2","name3","name4","name5"];
+let theme = ["Dino","Temp","Dino","Temp","Dino"];
+let bgFiles = ["dino.png","temp.png"];
+let time = new Date();
+
 
 //varriables related to csv file
 var file = "file.csv";
 
-document.getElementById("n1").innerHTML = childName[0];
+
+let childIndex = 0;
+//setname();
+
+
+function setname(){
+    
+    let bgElement = document.getElementById("bg");
+    let bgImage = "none";
+    let nameText = document.getElementById("n1");
+    childIndex++;
+    
+    if(childIndex >= childName.length){childIndex = 0}
+    if (theme[childIndex] == "Dino"){bgImage = bgFiles[0]}
+    if (theme[childIndex] == "Temp"){bgImage = bgFiles[1]}
+    nameText.innerHTML = childName[childIndex];
+    bgElement.src = bgImage;
+
+    setTimeout(setname, 5000);
+    
+}
+
+
+
