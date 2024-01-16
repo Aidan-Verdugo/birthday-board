e617c8930abc713a6074f6b9844a9f439cc34636
Aidan-Verdugo <aidan.william.verdugo@gmail.com>
2024-01-16 15:41:10 -0600

    added tabletesting php

---
+++ import/import-test.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $connection = pg_connect("host=localhost:8989 dbname=test user=postgres password=password");
+    if (!$connection){
+        echo "An error occurred. <br>";
+        exit;
+    }
+    ?>
+    
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Theme</th>
+            <th>Party Time</th>
+            <th>Party Day</th>
+        </tr>
+        <?php
+        while($row = pg_fetch_assoc($result)){
+            echo "
+            <tr>
+                <td>$row[name]</td>
+                <td>$row[theme]</td>
+                <td>$row[partytime]</td>
+                <td>$row[partyday]</td>
+            </tr>
+            ";
+        }
+        
+        ?>
+
+    </table>
+</body>
+</html>
